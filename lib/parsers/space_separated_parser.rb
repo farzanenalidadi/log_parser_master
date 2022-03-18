6d0ca998ac205f5789ba981d13ad0691225c6a30
farzanealidadi <farzanealidadi370@gmail.com>
2022-03-18 10:42:21 +0330

    Add class space_separated_parser with Rspec test (read logfile and set special index  into hash  with space separator)

---
+++ lib/parsers/space_separated_parser.rb
@@ -0,0 +1,52 @@
+# Use Set class for create unique list
+require 'set'
+
+# SpaceSeparatedParser give  filename as argument that this file contain page_url and ip in each line
+# In this class pars file line and validate line then calculate visit count and unique visit and append in hash
+class SpaceSeparatedParser
+  attr_reader :filename
+
+  def initialize(filename)
+    @filename = filename
+    @pars_data = {}
+    return if file_valid?(filename)
+
+    raise(ArgumentError, 'The file does not exist, or is empty!')
+  end
+
+  def parse
+    File.open(filename, 'r').each_line do |line|
+      line = line.strip
+      next unless line_valid?(line)
+
+      append_line(line)
+    end
+    
+    return @pars_data unless @pars_data.empty?
+
+    raise(ArgumentError, 'The file have  not any valid line!')
+  end
+
+  private
+
+  def file_valid?(filename)
+    return false unless File.exist?(filename)
+    return false if File.size(filename) <= 0
+
+    true
+  end
+
+  def line_valid?(line)
+    return false if !line || (line.length <= 0)
+    return false unless line.match?(%r{^/\w+(/\w+)*(/)? (\d{1,3}\.){3}\d{1,3}$}) # w:[a-zA-Z0-9_] ,d: [0-9]
+
+    true
+  end
+
+  def append_line(line)
+    url, ip = line.split
+    @pars_data[url] ||= { visits: 0, unique_visitors: Set.new }
+    @pars_data[url][:visits] += 1
+    @pars_data[url][:unique_visitors].add(ip)
+  end
+end
---
+++ spec/parsers/space_separated_parser_spec.rb
@@ -0,0 +1,42 @@
+require 'parsers/space_separated_parser'
+require 'tempfile' # Use Tempfile class  to create temporary file in test
+
+describe SpaceSeparatedParser do
+  describe '#parse' do
+    context 'with invalid data and failed' do
+      before :each do
+        @empty_file = Tempfile.new('empty_file')
+        File.write(@empty_file, '')
+
+        @invalid_file = Tempfile.new('invalid_file')
+        File.write(@invalid_file, 'This-Is-Invalid-Data')
+      end
+
+      it 'file does not exist' do
+        expect { described_class.new('dont_exit') }
+          .to(raise_error(ArgumentError))
+      end
+
+      it 'file is empty' do
+        expect { described_class.new(@empty_file) }
+          .to(raise_error(ArgumentError))
+      end
+
+      it 'file doesnt any valid line' do
+        expect { described_class.new(@invalid_file).parse }
+          .to(raise_error(ArgumentError))
+      end
+    end
+
+    context 'with valid data and success' do
+      subject { described_class.new('spec/fixtures/webserver.log').parse }
+      it 'should parse this file with valid line' do
+        mock_result = {
+          "/contact" => { unique_visitors: Set.new(["184.123.665.067"]), visits: 1 },
+          "/home" => { unique_visitors: Set.new(["184.123.665.067"]), visits: 1 }
+        }
+        expect(subject).to(eq(mock_result))
+      end
+    end
+  end
+end
