8faba9a0dfd13e4e85edd2090548388e584af64a
farzanealidadi <farzanealidadi370@gmail.com>
2022-03-19 10:30:18 +0330

    Add  class base sorter

---
+++ lib/sorters/sorter.rb
@@ -0,0 +1,26 @@
+class Sorter
+  include Enumerable
+
+  def initialize(parse_data)
+    @pars_data = parse_data
+    sort_out
+  end
+
+  def each
+    not_implemented!
+  end
+
+  def self.title
+    not_implemented!
+  end
+
+  private
+
+  def sort_out
+    not_implemented!
+  end
+
+  def not_implemented!
+    raise(NotImplementedError, 'A Sorter class cannot be used. Use a subclass')
+  end
+end
---
+++ spec/sorters/sorter_spec.rb
@@ -0,0 +1,24 @@
+require 'sorters/sorter'
+
+describe Sorter do
+  describe '#initialize' do
+    it 'should return NotImplementedError' do
+      expect { described_class.new({}) }
+        .to(raise_error(NotImplementedError))
+    end
+  end
+
+  describe '#each' do
+    it 'should return NotImplementedError' do
+      expect { described_class.new({}).each }
+        .to(raise_error(NotImplementedError))
+    end
+  end
+
+  describe '#title' do
+    it 'should return NotImplementedError' do
+      expect { described_class.new({}).title }
+        .to(raise_error(NotImplementedError))
+    end
+  end
+end
